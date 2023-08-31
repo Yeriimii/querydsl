2b0dd8570c1a6a45a860074653409475edcbc05e
Yeriimii <seojs0511@naver.com>
2023-08-31 13:11:01 +0900

    조회 API controller 개발

---
+++ src/main/java/study/querydsl/controller/InitMember.java
@@ -0,0 +1,48 @@
+package study.querydsl.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
+
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Profile("local")
+@Component
+@RequiredArgsConstructor
+public class InitMember {
+
+    private final InitMemberService initMemberService;
+
+    @PostConstruct
+    public void init() {
+        initMemberService.init();
+    }
+
+    /**
+     * @PostConstruct init() 내부에 멤버를 만드는 함수를 쓸 수 없는 이유: 스프링빈 라이프사이클 때문에
+     */
+    @Component
+    static class InitMemberService {
+        @PersistenceContext
+        private EntityManager em;
+
+        @Transactional
+        public void init() {
+            Team teamA = new Team("teamA");
+            Team teamB = new Team("teamB");
+            em.persist(teamA);
+            em.persist(teamB);
+
+            for (int i = 0; i < 100; i++) {
+                Team selectedTeam = 1 % 2 == 0 ? teamA : teamB;
+                em.persist(new Member("member" + i, i, selectedTeam));
+            }
+        }
+    }
+
+}
